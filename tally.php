c5f9a6e24a7b7677ab855e59f8d3d0c7c50ff2dd
cindy21td <m.cindy.prasetio@gmail.com>
2016-05-18 20:51:20 -0700

    Fixed the php entry

--- tally.php
+++ tally.php
@@ -18,48 +18,24 @@ try {
 $name = $_POST["name"];
 
 $item = $dbh->quote($_POST["item"]);
-$ticket = $dbh->quote($_POST["ticket"]);
 $cash = $dbh->quote($_POST["cash"]);
-$payment = $dbh->quote($_POST["payment"]);
-$password = $dbh->quote($_POST["password"]);
 $time = $dbh->quote($_POST["time"]);
 $comment = $dbh->quote($_POST["comment"]);
-
-$sql = "INSERT INTO :name
-      (item, ticket, cash, payment_type, password, transaction_time, comment) 
-      VALUES 
-      (:item, :ticket, :cash, :payment, :password, :time, :comment)";
-
-$stmt = $conn->prepare($sql);
-
-$stmt->bindParam(':name', $name, PDO::PARAM_STR); 
-$stmt->bindParam(':item', $item, PDO::PARAM_STR);    
-$stmt->bindParam(':ticket', $ticket, PDO::PARAM_INT); 
-$stmt->bindParam(':cash', $cash, PDO::PARAM_INT); 
-$stmt->bindParam(':payment', $payment, PDO::PARAM_STR);    
-$stmt->bindParam(':password', $password, PDO::PARAM_STR);    
-$stmt->bindParam(':time', $time, PDO::PARAM_STR);    
-$stmt->bindParam(':comment', $comment, PDO::PARAM_STR);    
-
-
 
 try {  
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   $dbh->beginTransaction();
   $sql = "INSERT INTO :name
-      (item, ticket, cash, payment_type, password, transaction_time, comment) 
+      (item, cash, transaction_time, comment) 
       VALUES 
-      (:item, :ticket, :cash, :payment, :password, :time, :comment)";
+      (:item, :cash, :time, :comment)";
 
-  $stmt = $dbh->prepare($sql);
+  $stmt = $conn->prepare($sql);
 
   $stmt->bindParam(':name', $name, PDO::PARAM_STR); 
   $stmt->bindParam(':item', $item, PDO::PARAM_STR);    
-  $stmt->bindParam(':ticket', $ticket, PDO::PARAM_INT); 
   $stmt->bindParam(':cash', $cash, PDO::PARAM_INT); 
-  $stmt->bindParam(':payment', $payment, PDO::PARAM_STR);    
-  $stmt->bindParam(':password', $password, PDO::PARAM_STR);    
   $stmt->bindParam(':time', $time, PDO::PARAM_STR);    
   $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);   
 
